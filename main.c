7a1ab8e25d8db01d56dfd2f7abdcf4e4653e6152
Érico Edú Corrêa <rico Ed Corra>
2016-09-02 22:20:34 -0300

    programa funcional

--- main.c
+++ main.c
@@ -1,8 +1,49 @@
+//---------------------------------------------------------------------------------------------
+//Programa exemplo da utilização de ponteiros
+//
+//Laboratório de Algoritmo II
+//
+//Érico Edú Corrêa, acadêmico do curso de Sistemas de Informação, da Faculdade Antonio Meneghetti
+//Data: 02/09/2016
+//---------------------------------------------------------------------------------------------
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main()
+{
+//Criação e inicialização das variaveis
+int Variavel_1 = 1, Variavel_2 = 2;
+char Controle = 'n', Descarga; 
 
-int main(int argc, char *argv[]) {
-	return 0;
+//Criação dos ponteiros
+int *Ponteiro_1, *Ponteiro_2;
+
+//enquanto 
+while (1)
+{
+	//Inicialização dos ponteiros 
+	Ponteiro_1 = &Variavel_1; //Ponteiro_1 recebe o endereço de Variavel_1
+	Ponteiro_2 = &Variavel_2; //Ponteiro_2 recebe o endereço de Variavel_2
+	
+	//Exibe e solicita de controle
+	printf("\n\nDESEJA FONALIZAR O PROGRAMA? (s/n)");
+	//Efetua a leitura do comando
+	scanf("%c", &Controle);
+	// Se o camando indicar a finalização, finalize o loop
+	if(Controle == 's') break;
+	
+	//Exibe a solicitação de um valor
+	printf("\nDIGITE UM VALOR PARA Variavel_1: ");
+	//Efetua a leitura do valor e transfere o valor para Variavel_1
+	scanf("%i", Ponteiro_1);
+	//Exibe a solicitação de um valor
+	printf("\nDIGITE UM VALOR PARA Variavel_2: ");
+	//Efetua a leitura do valor e transfere o valor para Variavel_2
+	//scanf("%i%c", Ponteiro_2, &Descarga);
+	scanf("%i", Ponteiro_2);
+}
+//retorna 0
+return 0;
 }
